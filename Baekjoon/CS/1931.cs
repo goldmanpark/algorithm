21f75d0fb7eadfaeb8025f23f03248a86011e55a
Goldman Park <death2501@gmail.com>
2020-09-19 21:18:16 +0900

    2nd Try -> TImeout Failed

--- Baekjoon/CS/1931.cs
+++ Baekjoon/CS/1931.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
+//2nd Try -> Timeout Failed
 namespace Solution
 {
     public class Solution_1931
@@ -9,35 +9,25 @@ namespace Solution
         public static void Main(string[] args)
         {
             int size = int.Parse(Console.ReadLine());
-            List<int[]> list = new List<int[]>();            
+            int[][] list = new int[size][];
 
             for(int i = 0 ; i < size ; i++)
             {
-                int [] temp = new int[2];
                 string tempStr = Console.ReadLine();
                 string [] tempStr2 = tempStr.Split(' ');
-                temp[0] = int.Parse(tempStr2[0]);
-                temp[1] = int.Parse(tempStr2[1]);
-                //temp[2] = temp[1] - temp[0];
-                
-                // if(!list.Contains(temp))
-                //     list.Add(temp);
+                int [] tempInt = new int[2];
+                tempInt[0] = int.Parse(tempStr2[0]);
+                tempInt[1] = int.Parse(tempStr2[1]);
+
+                if(!list.Contains(tempInt))
+                    list.Append(tempInt);
             }
 
             int answer = 1;
-            //List<int[]> answerList = new List<int[]>(); 
             int lastIdx = 0;
-            list = list.OrderBy(x => x[0]).ToList();
-            for(int i = 1 ; i < size ; i++)
+            list = list.OrderBy(x => x[1]).ToArray();
+            for(int i = 1 ; i < list.Length ; i++)
             {
-                // var nested = answerList.Where(x => ((x[0] < list[i][0] && list[i][0] < x[1]) || 
-                //                                     (x[0] < list[i][1] && list[i][1] < x[1]) || 
-                //                                     (x[0] > list[i][0] && list[i][1] > x[1]))).FirstOrDefault();
-                // if(nested == null)
-                // {
-                //     answer++;
-                //     answerList.Add(list[i]);
-                // }
                 if(list[lastIdx][1] < list[i][0])
                 {
                     answer++;
@@ -46,7 +36,6 @@ namespace Solution
             }
 
             Console.WriteLine(answer);
-            //answerList.ForEach((x) => {Console.WriteLine(string.Format(x[0].ToString() + " " + x[1]));});
         }
     }
 }
