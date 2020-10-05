8395e41c06e0a295e52d2661a0bfd9e7e9247db6
goldmanpark <death2501@gmail.com>
2020-10-05 17:43:04 +0900

    2020.10.05

---
+++ Baekjoon/Basic_Implementation/11654.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Solution
+{
+    public class Solution_11654
+    {
+        public static void Main(string[] args)
+        {
+            char ch = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(Convert.ToInt32(ch));
+        }
+    }
+}
