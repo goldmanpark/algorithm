7aa250551b274ed50499df01464a38533142b277
Goldman <death2501@gmail.com>
2020-10-23 16:27:54 +0900

    2020.10.23 Not Complete

--- Baekjoon/Dynamic_Programming/2579.cs
+++ Baekjoon/Dynamic_Programming/2579.cs
@@ -18,34 +18,62 @@ namespace Solution
     {
         static int[] stairs;
         static int[] memo;
-        static List<int> idxHist;
+        static List<int> idxHist;   //history of stepping on the stairs, storing steps(index of stairs).
 
         public static void Main(string[] args)
         {
             int N = int.Parse(Console.ReadLine());
             stairs = new int[N + 1];
             memo = new int[N + 1];
+            idxHist = new List<int>();
 
             for(int i = 1 ; i <= N ; i++){
                 stairs[i] = int.Parse(Console.ReadLine());
             }
             memo[1] = stairs[1];
             memo[2] = stairs[1] + stairs[2];
+            idxHist.Add(0);
+
             Console.WriteLine(GetSolution(N));
+            for(int i = 0 ; i <= N ; i++)
+                Console.Write(memo[i].ToString() + " ");
+            Console.WriteLine();
+            idxHist.ForEach((x) => { Console.Write(x.ToString() + " "); });
         }
 
         private static int GetSolution(int n)
         {
             if(memo[n] != 0)
                 return memo[n];
-            else{
+            else
+            {
                 int x = GetSolution(n - 1) + stairs[n];
                 int y = GetSolution(n - 2) + stairs[n];
-                int i = idxHist.Count;
 
-                if(i >= 2 && idxHist[i - 1] == idxHist[i - 2] + 1 && i == n){
-                    memo[n] = y;
-                    idxHist.Add(n - 2);
+                if(idxHist.Count >= 2)
+                {                    
+                    int lastIdx1 = idxHist[idxHist.Count - 1];
+                    int lastIdx2 = idxHist[idxHist.Count - 2];
+
+                    if(lastIdx1 == lastIdx2 + 1){
+                        if(n - 1 == lastIdx1 + 1){
+                            memo[n] = x >= y ? x : y;
+                        }
+                        else{
+                            memo[n] = x;
+                            idxHist.Add(n - 1);
+                        }
+                    }
+                    else{
+                        if(x >= y){
+                            memo[n] = x;
+                            idxHist.Add(n - 1);
+                        }
+                        else{
+                            memo[n] = y;
+                            idxHist.Add(n - 2);
+                        }
+                    }
                 }
                 else{
                     if(x >= y){
