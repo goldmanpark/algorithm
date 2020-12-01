3f040cd359f00fe98bff879b8d816aa6b7b94742
Goldman <death2501@gmail.com>
2020-12-01 17:49:37 +0900

    2020.12.01 timeout

--- Baekjoon/Number_Theory/1929.py
+++ Baekjoon/Number_Theory/1929.py
@@ -1,23 +1,30 @@
 import sys
+import math
 
 # Sieve of Eratosthenes
-# 2020.11.30 timeout
 
 val = sys.stdin.readline().split()
 M = int(val[0])
 N = int(val[1])
 answer = list(range(M, N + 1))
+
+# 1. remove 1
 if answer[0] == 1:
     del answer[0]
+# 2. remove multiples of 2
+for x in answer:
+    if x % 2 == 0:
+        answer.remove(x)
+# 3. remove multiples of odd numbers(3 ~ sqrt(N))
+odd = 3
+sqrt = int(math.sqrt(N))
+while odd <= sqrt:
+    for x in answer:
+        if x == odd:
+            continue
+        if x % odd == 0:
+            answer.remove(x)
+    odd += 2
 
-for i in range(2, N):
-    for j in range(2, N):
-        notPrime = i * j
-        if notPrime > N:
-            break
-        elif notPrime in answer:
-            answer.remove(notPrime)
-
-for element in answer:
-    print(element)
-        
+for x in answer:
+    print(x)
