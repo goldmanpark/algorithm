62d5f8b03f55a668f159b7667964756f06f5c81b
Goldman <death2501@gmail.com>
2020-12-18 16:03:48 +0900

    2020.12.18 Complete
    Sieve of Eratosthenes

--- Baekjoon/Number_Theory/1929.py
+++ Baekjoon/Number_Theory/1929.py
@@ -6,25 +6,36 @@ import math
 val = sys.stdin.readline().split()
 M = int(val[0])
 N = int(val[1])
-answer = list(range(M, N + 1))
+if M == N == 1:
+    sys.exit(0)
+numbers = list(range(M, N + 1))
+isPrime = [True for x in range(M, N + 1)]
+startIdx = 0    # 2,3,4,5.. startIdx = 0, 1,2,3,4..startIdx = 0
 
 # 1. remove 1
-if answer[0] == 1:
-    del answer[0]
+if numbers[0] == 1:
+    del isPrime[0]
+    del numbers[0]
+
 # 2. remove multiples of 2
-for x in answer:
-    if x % 2 == 0:
-        answer.remove(x)
+startIdx = 0 if numbers[0] % 2 == 0 else 1
+for i in range(startIdx, len(numbers), 2):
+    isPrime[i] = False
+if numbers[0] == 2: # 2 is the only even prime number
+    isPrime[0] = True
+
 # 3. remove multiples of odd numbers(3 ~ sqrt(N))
 odd = 3
 sqrt = int(math.sqrt(N))
 while odd <= sqrt:
-    for x in answer:
-        if x == odd:
+    startIdx = 1 if numbers[0] % 2 == 0 else 0
+    for i in range(startIdx, len(numbers), 2):
+        if numbers[i] == odd:
             continue
-        if x % odd == 0:
-            answer.remove(x)
+        if numbers[i] % odd == 0:
+            isPrime[i] = False
     odd += 2
 
-for x in answer:
-    print(x)
+for i in range(len(numbers)):
+    if isPrime[i] == True:
+        print(numbers[i])
