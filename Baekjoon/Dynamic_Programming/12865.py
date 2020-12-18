11d67f15ff32177a3d9884061a96f7fbb4acf2e3
Goldman <death2501@gmail.com>
2020-12-18 11:09:46 +0900

    2020.12.18 knapsack complete

--- Baekjoon/Dynamic_Programming/12865.py
+++ Baekjoon/Dynamic_Programming/12865.py
@@ -1,28 +1,31 @@
 import sys
 
-# knapsack problem
 '''
-    memo[i][w] = max value, considering item 1 ~ i and max weight 'w'
-    memo[i][w] = max(
-        memo[i-1][w],                                 # if the i-th item is not put
-        memo[i-1][w - item[i].weight] + item[i].value # if the i-th item is put
+    # the knapsack problem
+    memo[w][i] = max value, considering knapsack loadable weight is 'w' and item 1 ~ i
+    memo[w][i] = max(
+        memo[w][i-1],                       # if the i-th item is not put
+        memo[w - weight[i]][i-1] + value[i] # if the i-th item is put
         )
 '''
 
-class Item:
-    def __init__(self, w, v):
-        self.weight = w
-        self.value = v
-
 inputStr = sys.stdin.readline().split()
-N = int(inputStr[0]) # number of items
-K = int(inputStr[1]) # maximum weight
-items = []
+N = int(inputStr[0])                    # number of items
+K = int(inputStr[1])                    # maximum weight
+weight = [0 for i in range(N + 1)]      # index of items: 1 ~ N
+value = [0 for i in range(N + 1)]
+memo = [[0 for col in range(N + 1)] for row in range(K + 1)]
 
-
-for i in range(N):
+for i in range(1, N + 1):
     inputStr = sys.stdin.readline().split()
-    item = Item(inputStr[0], inputStr[1])
-    items.append(item)
+    weight[i] = int(inputStr[0])
+    value[i] = int(inputStr[1])
 
-items.sort(key=lambda x: x.weight)
+for w in range(1, K + 1):       # pivot: loadable weight
+    for i in range(1, N + 1):
+        if weight[i] > w:
+            memo[w][i] = memo[w][i - 1]
+        else:
+            memo[w][i] = max(memo[w][i - 1], memo[w - weight[i]][i - 1] + value[i])
+
+print(memo[K][N])
