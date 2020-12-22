f398b6bf4f7cbe2b17d0e33e722daf6cd5f5a4da
Goldman <death2501@gmail.com>
2020-12-22 19:33:16 +0900

    2020.12.22 Dijkstra algorithm

--- Baekjoon/Shortest_Path/1753.py
+++ Baekjoon/Shortest_Path/1753.py
@@ -1,18 +1,36 @@
 import sys
 import heapq as hq
 
-def dijkstra(end):
-    queue = []
-    
+# Dijkstra algorithm using priority queue
 
-inputStr = sys.stdin.readline().split()
-V = int(inputStr[0])
-E = int(inputStr[1])
-graph = [[0 for col in range(V + 1)] for row in range(V + 1)]
+input = sys.stdin.readline
+INF = sys.maxsize
 
-for i in range(E):
-    inputStr = sys.stdin.readline().split()
-    u = int(inputStr[0])
-    v = int(inputStr[1])
-    w = int(inputStr[2])
-    graph[u][v] = w
+V, E = map(int, input().split())
+K = int(input())
+graph = [[] for row in range(V + 1)]
+distance = [INF] * (V + 1)
+
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w)) # from u to v, weight w
+
+heap = []                   # min heap, tuple: (distance, vertex)
+hq.heappush(heap, (0, K))   # push start vertex
+distance[K] = 0
+
+while len(heap) > 0:
+    dist, vertex = hq.heappop(heap)
+    if distance[vertex] < dist:
+        continue
+    for edge in graph[vertex]:  # edge[0] = target vertex
+        cost = dist + edge[1]   # edge[1] = weight
+        if cost < distance[edge[0]]:
+            distance[edge[0]] = cost
+            hq.heappush(heap, (cost, edge[0]))
+
+for i in range(1, V + 1):
+    if distance[i] != INF:
+        print(distance[i])
+    else:
+        print('INF')
